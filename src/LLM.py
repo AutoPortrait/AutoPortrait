5012401c93a30266eb61d8a938eb98460e3ced3c
nictheboy <nictheboy@outlook.com>
2024-12-09 20:53:51 +0800

    把智谱大模型的参数改成可修改了

--- src/LLM.py
+++ src/LLM.py
@@ -13,21 +13,28 @@ class LLMAbstract(Protocol):
     # May raise LLMProcessError
     def process(self, messages) -> str: ...
 
+    # May raise LLMProcessError
+    def batch_process(self, messages) -> list[str]: ...
+
 
 class LLMZhipuAI:
-    def __init__(self):
+    def __init__(self, model_name: str, top_p: float, temperature: float, max_tokens: int):
         load_dotenv()
         zhipu_key = os.getenv("ZHIPU_KEY")
         self.client = ZhipuAI(api_key=zhipu_key)
+        self.model_name = model_name
+        self.top_p = top_p
+        self.temperature = temperature
+        self.max_tokens = max_tokens
 
     def process(self, messages) -> str:
         try:
             result = self.client.chat.completions.create(
-                model="GLM-4-Air",
+                model=self.model_name,
                 messages=messages,
-                top_p=0.7,
-                temperature=0.80,
-                max_tokens=4095,
+                top_p=self.top_p,
+                temperature=self.temperature,
+                max_tokens=self.max_tokens,
                 stream=False,  # 关闭流模式，直接接收完整响
             )
             return result.choices[0].message.content
@@ -35,4 +42,4 @@ class LLMZhipuAI:
             raise LLMProcessError(e)
 
 
-LLMCurrent: LLMAbstract = LLMZhipuAI()
+LLMCurrent: LLMAbstract = LLMZhipuAI("GLM-4-Air", 0.7, 0.80, 4095)
